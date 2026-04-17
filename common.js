27e49af9c5c44906321f3575a5d6ca8ab16ad084
sishakk-cmd <sishakk@gmail.com>
2026-04-17 11:26:12 +0530

    Update common.js

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://script.google.com/macros/s/AKfycbw2g7iajb_sTrAdjZYZW0WUkoODckWgqh196b4gpxQoTraP6w2dAHfQ8iNtaozLmEa3/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbwIXrSO-1SxbZZws3ffzobOsjYSG-IxHzfV4pbxJXS7stRfN5Vfb2RMt7tTOVp2LTEq/exec";
 
 async function apiFetch(payload) {
   // payload is an object with at least `action` property
